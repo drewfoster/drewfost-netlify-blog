bdb1d7a64cb5c7e99405f58b6b5f41f74da4ff5d
drewfoster <drewfoster@hotmail.co.uk>
2019-01-04 21:30:20 +1100

    Changes to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,17 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-transparent has-text-centered">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Andrew Foster" style={{ width: '88px' }} />
+            <img src={logo} alt="Andrew Foster" style={{ width: '88px', marginRight: '0.4em' }} />
           </figure>
-          <h1>  Andrew Foster</h1>
+          <h1>Andrew Foster | Financial Services Blog</h1>
         </Link>
-      </div>
-      <div className="navbar-start">
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/drewfoster/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
